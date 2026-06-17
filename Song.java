bf26e74d73d590670a62fd73e06e24d13febe5dc
mackleye8818 <mackleye8818@my.uwstout.edu>
2026-06-16 19:37:00 -0500

    Create Song.java

---
+++ Song.java
@@ -0,0 +1,32 @@
+public class Song extends RetailItem {
+    private String artist;
+    private int length;
+
+    public Song(String name, String artist, int length) {
+        super(name);
+        this.artist = artist;
+        this.length = length;
+    }
+
+    public Song(String name, int stars, String artist, int length) {
+        super(name, stars);
+        this.artist = artist;
+        this.length = length;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    @Override
+    public String toString() {
+        int minutes = length / 60;
+        int seconds = length % 60;
+        return "Song Name: " + getName() + ", Stars: " + getStars() +
+               "\nArtist: " + artist + ", Length: " + minutes + ":" + String.format("%02d", seconds);
+    }
+}
