fb7c63c5fd22be21b4389fa751a3d50d846836d8
mackleye8818 <mackleye8818@my.uwstout.edu>
2026-06-16 19:38:02 -0500

    Update Song.java

--- Song.java
+++ Song.java
@@ -1,28 +1,22 @@
 public class Song extends RetailItem {
     private String artist;
     private int length;
-
     public Song(String name, String artist, int length) {
         super(name);
         this.artist = artist;
         this.length = length;
     }
-
     public Song(String name, int stars, String artist, int length) {
         super(name, stars);
         this.artist = artist;
         this.length = length;
     }
-
     public String getArtist() {
         return artist;
     }
-
     public int getLength() {
         return length;
-    }
-
-    @Override
+    }    @Override
     public String toString() {
         int minutes = length / 60;
         int seconds = length % 60;
